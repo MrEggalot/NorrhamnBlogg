e1a817d0ea2072b253e84c7da352d3bbc251755d
Giedre Aleksandrauskaite <giedre711120@gmail.com>
2018-06-21 18:16:49 +0200

    can render html now

--- flaskServer/server.py
+++ flaskServer/server.py
@@ -1,10 +1,19 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "Giedres Server!"
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return render_template('hello.html')
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
